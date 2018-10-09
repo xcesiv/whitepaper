2843c4ec14dd5a3bfd065e43db96fa7be3cda739
xcesiv <xcesivio@gmail.com>
2018-10-09 10:52:25 -0400

    fix gh pages link

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -10,7 +10,7 @@ git checkout --orphan gh-pages
 git rm --cached -r -f .
 echo "# Automatic build" > README.md
 echo "Built pdf from \`$SHA\`. See https://github.com/durachain/whitepaper/ for details." >> README.md
-echo "The generated pdf is here: https://durachain.github.io/whitepaper.pdf" >> README.md
+echo "The generated pdf is here: https://durachain.github.io/whitepaper/whitepaper.pdf" >> README.md
 echo '<html><head><meta http-equiv="refresh" content="0; url=whitepaper.pdf" /></head><body></body></html>' > index.html
 mv build/whitepaper.pdf whitepaper.pdf
 git add -f README.md index.html whitepaper.pdf
