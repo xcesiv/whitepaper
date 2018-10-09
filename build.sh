e9b7e245c42e711278d39f5610d0588ab83dfe7c
xcesiv <xcesivio@gmail.com>
2018-10-09 01:32:11 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -34,9 +34,10 @@ then
 fi
 
 mkdir -p build
+
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
+bibtex build/whitepaper && \
 bibtex build/whitepaper && \
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex
-rm -rf Options.tex
