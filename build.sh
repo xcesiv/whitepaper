bf98f0a1cd1346ac59098f977bdefd61dc684323
swaevior <swaevior@gamerammo.io>
2018-10-09 10:33:56 -0400

    issue #6 revisions

--- build.sh
+++ build.sh
@@ -36,9 +36,5 @@ fi
 mkdir -p build
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
 bibtex build/whitepaper && \
-bibtex build/whitepaper && \
-bibtex build/whitepaper && \
-pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
-pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
 pdflatex -output-directory=build -interaction=errorstopmode -halt-on-error whitepaper.tex && \
 rm -rf Options.tex
